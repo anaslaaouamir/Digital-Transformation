8fb0a35d92f12a57c4b81a6b394719689125b3b6
Sabir <sabirzidane0@gmail.com>
2026-02-13 11:01:14 +0100

    problemes security resolved

--- Back/Admin/src/main/java/com/stage/admin/config/SecurityConfig.java
+++ Back/Admin/src/main/java/com/stage/admin/config/SecurityConfig.java
@@ -3,15 +3,22 @@ package com.stage.admin.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
-//@Configuration
+@Configuration
 public class SecurityConfig {
-/*
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-            .csrf(csrf -> csrf.disable())   // ← مهم بزاف
+            .csrf(csrf -> csrf.disable())
             .authorizeHttpRequests(auth -> auth
                 .requestMatchers("/api/**").permitAll()
                 .anyRequest().permitAll()
@@ -21,7 +28,4 @@ public class SecurityConfig {
 
         return http.build();
     }
-
-
- */
 }
