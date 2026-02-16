84449e9fec4f7c163eaadd679474f04204dae838
T470 <anasoffic2907@gmail.com>
2026-02-16 12:03:20 +0000

    admin edit profile

--- Back/Admin/src/main/java/com/stage/admin/dto/AdminUpdateRequest.java
+++ Back/Admin/src/main/java/com/stage/admin/dto/AdminUpdateRequest.java
@@ -1,4 +1,21 @@
 package com.stage.admin.dto;
 
+import lombok.*;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+
+@Data
 public class AdminUpdateRequest {
+    private String nom;
+    private String prenom;
+    private String email;
+    private String telephone;
+
+    // Mandatory: User must provide this to confirm identity
+    private String currentPassword;
+
+    // Optional: Only provide if they want to change it
+    private String newPassword;
 }
